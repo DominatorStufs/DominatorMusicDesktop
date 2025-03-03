36eb6994af04a97e95c4842b5856d34de0b9ebf9
DominatorStufs <dpdangergameryt@gmail.com>
2025-03-03 16:28:02 +0530

    Update player.jsx

--- components/cards/player.jsx
+++ components/cards/player.jsx
@@ -1,13 +1,15 @@
 "use client";
 import { useContext, useEffect, useRef, useState } from "react";
 import { Button } from "../ui/button";
-import { ExternalLink, Link2Icon, Pause, Play, Repeat, Repeat1, X } from "lucide-react";
+import { ExternalLink, Link2Icon, Pause, PauseCircle, Play, Repeat, Repeat1, X } from "lucide-react";
 import { Slider } from "../ui/slider";
 import { getSongsById } from "@/lib/fetch";
 import Link from "next/link";
 import { MusicContext } from "@/hooks/use-context";
 import { toast } from "sonner";
 import { Skeleton } from "../ui/skeleton";
+import { IoPause } from "react-icons/io5";
+import { useMusic } from "../music-provider";
 
 export default function Player() {
     const [data, setData] = useState([]);
@@ -56,25 +58,21 @@ export default function Player() {
     const loopSong = () => {
         audioRef.current.loop = !audioRef.current.loop;
         setIsLooping(!isLooping);
-        if (isLooping) {
-            toast.success('Removed from Loop!');
-        } else {
-            toast.success('Added to Loop!');
-        }
     };
 
+    const { current, setCurrent } = useMusic();
     useEffect(() => {
         if (values.music) {
             getSong();
-            if (localStorage.getItem("c")) {
-                audioRef.current.currentTime = parseFloat(localStorage.getItem("c") + 1);
-                localStorage.removeItem("c");
+            if (current) {
+                audioRef.current.currentTime = parseFloat(current + 1);
             }
             setPlaying(localStorage.getItem("p") == "true" && true || !localStorage.getItem("p") && true);
             const handleTimeUpdate = () => {
                 try {
                     setCurrentTime(audioRef.current.currentTime);
                     setDuration(audioRef.current.duration);
+                    setCurrent(audioRef.current.currentTime);
                 }
                 catch (e) {
                     setPlaying(false);
@@ -91,46 +89,40 @@ export default function Player() {
     return (
         <main>
             <audio autoPlay={playing} onPlay={() => setPlaying(true)} onPause={() => setPlaying(false)} onLoadedData={() => setDuration(audioRef.current.duration)} src={audioURL} ref={audioRef}></audio>
-            {values.music && <div className="shadow-lg fixed flex items-center bottom-0 right-0 left-0 border-border overflow-hidden border-t z-50 bg-background p-3 gap-3">
-                <div className="relative">
-                    <Button size="icon" variant="secondary" className="h-full w-full bg-secondary/30 hover:bg-secondary/50 backdrop-blur-sm absolute z-10" onClick={togglePlayPause}>{playing ? <Pause className="h-6 w-6" /> : <Play className="h-6 w-6" />}</Button>
-                    <img src={data.image ? data?.image[1]?.url : ""} alt={data?.name} className="rounded-md h-20 min-w-20 hover:opacity-85 transition" />
-                    <img src={data.image ? data?.image[1]?.url : ""} alt={data?.name} className="rounded-md h-[110%] min-w-[110%] opacity-40 hidden dark:block absolute top-0 left-0 right-0 blur-3xl -z-10" />
-                </div>
+            {values.music && <div className="shadow-lg fixed grid bottom-0 max-w-[500px] md:border-l md:border-r md:rounded-md md:!rounded-b-none md:ml-auto right-0 left-0 border-border overflow-hidden border-t-none z-50 bg-background gap-3">
                 <div className="w-full">
-                    <div className="flex items-center justify-between mb-2 w-full">
-                        <div>
-                            {!data?.name ? <Skeleton className="h-4 w-32" /> : (
-                                <>
-                                    <Link href={`/${values.music}?c=${currentTime}`} className="text-base hover:opacity-85 transition font-medium flex md:hidden gap-2 items-center">{data?.name?.slice(0, 10)}{data?.name?.length >= 11 ? ".." : ""}<ExternalLink className="h-3.5 w-3.5 text-muted-foreground" /></Link>
-                                    <Link href={`/${values.music}?c=${currentTime}`} className="text-base hover:opacity-85 transition font-medium gap-2 items-center hidden md:flex">{data?.name}<ExternalLink className="h-3.5 w-3.5 text-muted-foreground" /></Link>
-                                </>
-                            )}
-                            {!data?.artists?.primary[0]?.name ? <Skeleton className="h-3 w-14 mt-1" /> : (
-                                <>
-                                    <h2 className="block md:hidden text-xs -mt-0.5 text-muted-foreground">{data?.artists?.primary[0]?.name.slice(0, 20)}{data?.artists?.primary[0]?.name.length > 20 ? ".." : ""}</h2>
-                                    <h2 className="hidden md:block text-xs -mt-0.5 text-muted-foreground">{data?.artists?.primary[0]?.name}</h2>
-                                </>
-                            )}
+                    {!duration ? <Skeleton className="h-1 w-full" /> : (
+                        <Slider thumbClassName="hidden" trackClassName="h-1 transition-[height] group-hover:h-2 rounded-none" onValueChange={handleSeek} value={[currentTime]} max={duration} className="w-full group" />
+                    )}
+                </div>
+                <div className="grid gap-2 p-3 pt-0">
+                    <div className="flex items-center justify-between gap-3">
+                        <div className="relative flex items-center gap-2 w-full">
+                            <img src={data.image ? data?.image[1]?.url : ""} alt={data?.name} className="rounded-md aspect-square h-12 w-12 bg-secondary hover:opacity-85 transition cursor-pointer" />
+                            <img src={data.image ? data?.image[1]?.url : ""} alt={data?.name} className="rounded-md h-[110%] min-w-[110%] opacity-40 hidden dark:block absolute top-0 left-0 right-0 blur-3xl -z-10" />
+                            <div>
+                                {!data?.name ? <Skeleton className="h-4 w-32" /> : (
+                                    <>
+                                        <Link href={`/${values.music}`} className="text-base hover:opacity-85 transition font-medium flex md:hidden gap-2 items-center">{data?.name?.slice(0, 10)}{data?.name?.length >= 11 ? ".." : ""}<ExternalLink className="h-3.5 w-3.5 text-muted-foreground" /></Link>
+                                        <Link href={`/${values.music}`} className="text-base hover:opacity-85 transition font-medium gap-2 items-center hidden md:flex">{data?.name}<ExternalLink className="h-3.5 w-3.5 text-muted-foreground" /></Link>
+                                    </>
+                                )}
+                                {!data?.artists?.primary[0]?.name ? <Skeleton className="h-3 w-14 mt-1" /> : (
+                                    <>
+                                        <h2 className="block md:hidden text-xs -mt-0.5 text-muted-foreground">{data?.artists?.primary[0]?.name.slice(0, 20)}{data?.artists?.primary[0]?.name.length > 20 ? ".." : ""}</h2>
+                                        <h2 className="hidden md:block text-xs -mt-0.5 text-muted-foreground">{data?.artists?.primary[0]?.name}</h2>
+                                    </>
+                                )}
+                            </div>
                         </div>
-                        <div className="flex items-center gap-1.5 -mt-3.5">
-                            <Button size="icon" className="p-0 h-8 w-8" variant={!isLooping ? "ghost" : "secondary"} onClick={loopSong}>
+                        <div className="flex items-center gap-2">
+                            <Button size="icon" className="p-0 h-9 w-9" variant={!isLooping ? "ghost" : "secondary"} onClick={loopSong}>
                                 {!isLooping ? <Repeat className="h-3.5 w-3.5" /> : <Repeat1 className="h-3.5 w-3.5" />}
                             </Button>
-                            <Button size="icon" className="p-0 h-8 w-8" variant="outline" onClick={() => { values.setMusic(null); localStorage.clear(); audioRef.current.currentTime = 0; audioRef.current.src = null; setAudioURL(null); }}>
+                            <Button size="icon" className="p-0 h-9 w-9" onClick={togglePlayPause}>{playing ? <IoPause className="h-4 w-4" /> : <Play className="h-4 w-4" />}</Button>
+                            <Button size="icon" className="p-0 h-9 w-9" variant="secondary" onClick={() => { values.setMusic(null); setCurrent(0); localStorage.clear(); audioRef.current.currentTime = 0; audioRef.current.src = null; setAudioURL(null); }}>
                                 <X className="h-3.5 w-3.5" />
                             </Button>
-                        </div>
-                    </div>
-                    <div className="w-full grid gap-1">
-                        {!duration ? <Skeleton className="h-2 w-full" /> : (
-                            <Slider onValueChange={handleSeek} value={[currentTime]} max={duration} className="w-full" />
-                        )}
-                        <div className="flex items-center justify-between">
-                            <span className="text-[10px] font-light text-muted-foreground">{formatTime(currentTime)}</span>
-                            {!duration ? <Skeleton className="h-2 w-10" /> : (
-                                <span className="text-[10px] font-light text-muted-foreground">{formatTime(duration)}</span>
-                            )}
                         </div>
                     </div>
                 </div>
