290d120f117a012ecb060abf63c126469067cc2f
DominatorStufs <dpdangergameryt@gmail.com>
2025-03-03 20:31:55 +0530

    Update footer.jsx

--- components/page/footer.jsx
+++ components/page/footer.jsx
@@ -6,12 +6,11 @@ export default function Footer() {
             {/* <div>
                 <h1 className="text-xl font-bold">Music<span className="opacity-50">hub</span></h1>
             </div> */}
-            <p className="text-center text-muted-foreground text-sm mt-2 max-w-lg font-light">Built for educational purpose.</p>
-            <p className="text-center text-sm text-muted-foreground">Made with ♥ by <a className="underline text-primary hover:text-primary" href="https://www.instagram.com/king.onscreen">☠️king☠️</a>.</p>
-            <div className="flex gap-3 items-center justify-center mt-3">
-                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://whatsapp.com/channel/0029Vaf3fl4DZ4LTzJhwg00g">Source Code</Link>
+            <p className="text-sm text-muted-foreground">Built for educational purpose. Made with ♥ by <a className="underline text-primary hover:text-primary" href="https://github.com/DominatorStufs">☠️king☠️</a>.</p>
+            <div className="flex gap-3 items-center mt-3">
+                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://github.com/DominatorStufs/DominatorMusicDesktop">Source Code</Link>
                 <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://dominatorstufs.github.io/Portfolio/">Portfolio</Link>
-                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://www.instagram.com/dominator.onscreen">Instagram</Link>
+                <Link target="_blank" className="text-sm opacity-80 font-light underline hover:opacity-100" href="https://instagram.com/dom1nator.xyz">Instagram</Link>
             </div>
         </footer>
     )
