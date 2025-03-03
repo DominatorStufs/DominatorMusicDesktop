accdb2aa23ec6b1d25d826e66b6cd91f24ff6940
DominatorStufs <dpdangergameryt@gmail.com>
2025-03-03 19:36:24 +0530

    Update Player.jsx

--- app/(player)/_components/Player.jsx
+++ app/(player)/_components/Player.jsx
@@ -10,6 +10,8 @@ import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { NextContext } from "@/hooks/use-context";
 import Next from "@/components/cards/next";
+import { useMusic } from "@/components/music-provider";
+import { IoPause } from "react-icons/io5";
 
 export default function Player({ id }) {
     const [data, setData] = useState([]);
@@ -22,6 +24,7 @@ export default function Player({ id }) {
     const [audioURL, setAudioURL] = useState("");
     const params = useSearchParams();
     const next = useContext(NextContext);
+    const { current, setCurrent } = useMusic();
 
     const getSong = async () => {
         const get = await getSongsById(id);
@@ -76,11 +79,6 @@ export default function Player({ id }) {
     const loopSong = () => {
         audioRef.current.loop = !audioRef.current.loop;
         setIsLooping(!isLooping);
-        if (isLooping) {
-            toast.success('Removed from Loop!');
-        } else {
-            toast.success('Added to Loop!');
-        }
     };
 
     const handleShare = () => {
@@ -99,14 +97,14 @@ export default function Player({ id }) {
         getSong();
         localStorage.setItem("last-played", id);
         localStorage.removeItem("p");
-        if (params.get("c")) {
-            audioRef.current.currentTime = parseFloat(params.get("c") + 1);
+        if (current) {
+            audioRef.current.currentTime = parseFloat(current + 1);
         }
         const handleTimeUpdate = () => {
             try {
                 setCurrentTime(audioRef.current.currentTime);
                 setDuration(audioRef.current.duration);
-                localStorage.setItem("c", audioRef.current.currentTime);
+                setCurrent(audioRef.current.currentTime);
             }
             catch (e) {
                 setPlaying(false);
@@ -129,7 +127,7 @@ export default function Player({ id }) {
         return handleRedirect();
     }, [currentTime, duration, isLooping, next?.nextData?.id]);
     return (
-        <div className="mb-3 mt-4">
+        <div className="mb-3 mt-10">
             <audio onPlay={() => setPlaying(true)} onPause={() => setPlaying(false)} onLoadedData={() => setDuration(audioRef.current.duration)} autoPlay={playing} src={audioURL} ref={audioRef}></audio>
             <div className="grid gap-6 w-full">
                 <div className="sm:flex px-6 md:px-20 lg:px-32 grid gap-5 w-full">
@@ -138,7 +136,7 @@ export default function Player({ id }) {
                             <Skeleton className="md:w-[130px] aspect-square rounded-2xl md:h-[150px]" />
                         ) : (
                             <div className="relative">
-                                <img src={data.image[2].url} className="sm:h-[150px] h-full bg-secondary/50 rounded-2xl sm:w-[200px] w-full object-cover" />
+                                <img src={data.image[2].url} className="sm:h-[150px] h-full aspect-square bg-secondary/50 rounded-2xl sm:w-[200px] w-full sm:mx-0 mx-auto object-cover" />
                                 <img src={data.image[2].url} className="hidden dark:block absolute top-0 left-0 w-[110%] h-[110%] blur-3xl -z-10 opacity-50" />
                             </div>
                         )}
@@ -164,37 +162,38 @@ export default function Player({ id }) {
                         </div>
                     ) : (
                         <div className="flex flex-col justify-between w-full">
-                            <div>
-                                <h1 className="text-xl font-bold md:max-w-lg max-w-[260px]">{data.name}</h1>
-                                <p className="text-xs text-muted-foreground">by <Link href={"/search/" + `${encodeURI(data.artists.primary[0].name.toLowerCase().split(" ").join("+"))}`} className="text-foreground">{data.artists.primary[0]?.name || "unknown"}</Link></p>
+                            <div className="sm:mt-0 mt-3">
+                                <h1 className="text-xl font-bold md:max-w-lg">{data.name}</h1>
+                                <p className="text-sm text-muted-foreground">by <Link href={"/search/" + `${encodeURI(data.artists.primary[0].name.toLowerCase().split(" ").join("+"))}`} className="text-foreground">{data.artists.primary[0]?.name || "unknown"}</Link></p>
                             </div>
                             <div className="grid gap-2 w-full mt-5 sm:mt-0">
                                 <Slider onValueChange={handleSeek} value={[currentTime]} max={duration} className="w-full" />
                                 <div className="w-full flex items-center justify-between">
-                                    <span className="text-xs">{formatTime(currentTime)}</span>
-                                    <span className="text-xs">{formatTime(duration)}</span>
+                                    <span className="text-sm">{formatTime(currentTime)}</span>
+                                    <span className="text-sm">{formatTime(duration)}</span>
                                 </div>
-                                <div className="flex items-center justify-between mt-2">
-                                    <div className="flex items-center gap-3 sm:mt-0">
-                                        <Button size="icon" variant="outline" onClick={loopSong}>
+                                <div className="flex items-center mt-1 justify-between w-full sm:mt-2">
+                                    <Button variant={playing ? "default" : "secondary"} className="gap-1 rounded-full" onClick={togglePlayPause}>
+                                        {playing ? (
+                                            <IoPause className="h-4 w-4" />
+                                        ) : (
+                                            <Play className="h-4 w-4" />
+                                        )}
+                                        {playing ? "Pause" : "Play"}
+                                    </Button>
+                                    <div className="flex items-center gap-2 sm:gap-3 sm:mt-0">
+                                        <Button size="icon" variant="ghost" onClick={loopSong}>
                                             {!isLooping ? <Repeat className="h-4 w-4" /> : <Repeat1 className="h-4 w-4" />}
                                         </Button>
-                                        <Button size="icon" variant={playing ? "gooeyRight" : "gooeyLeft"} onClick={togglePlayPause}>
-                                            {playing ? (
-                                                <Pause className="h-4 w-4" />
-                                            ) : (
-                                                <Play className="h-4 w-4" />
-                                            )}
-                                        </Button>
-                                        <Button size="icon" variant="outline" onClick={downloadSong}>
+                                        <Button size="icon" variant="ghost" onClick={downloadSong}>
                                             {isDownloading ? (
                                                 <Loader2 className="h-4 w-4 animate-spin" />
                                             ) : (
                                                 <Download className="h-4 w-4" />
                                             )}
                                         </Button>
+                                        <Button size="icon" variant="ghost" onClick={handleShare}><Share2 className="h-4 w-4" /></Button>
                                     </div>
-                                    <Button size="icon" variant="outline" onClick={handleShare}><Share2 className="h-4 w-4" /></Button>
                                 </div>
                             </div>
                         </div>
@@ -202,7 +201,9 @@ export default function Player({ id }) {
                 </div>
             </div>
             {next.nextData && (
-                <Next name={next.nextData.name} artist={next.nextData.artist} image={next.nextData.image} />
+                <div className="mt-10 -mb-3 px-6 md:px-20 lg:px-32">
+                    <Next name={next.nextData.name} artist={next.nextData.artist} image={next.nextData.image} id={next.nextData.id} />
+                </div>
             )}
         </div>
     )
